25f4d3f43bb28a34df4a17ea7f8c8b673f01be90
Ksenia Akimova <ksenia_a02@mail.ru>
2025-03-22 23:21:48 +0300

    добавлен drawer из antd

--- OneDrive/Desktop/XmasShop/my-next-app/app/components/layout/header/cart/Cart.tsx
+++ OneDrive/Desktop/XmasShop/my-next-app/app/components/layout/header/cart/Cart.tsx
@@ -1,20 +1,62 @@
-import { Badge, Card, Space } from "antd";
-import { FC } from "react";
+// import { Badge, Button, Card, Drawer, Space } from "antd";
+// import { FC, useState } from "react";
+// import styles from "./Cart.module.scss";
+// import CartItem from "./cart-item/CartItem";
+// import { cart } from "@/app/data/cart.data";
+// const Cart: FC = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   return (
+//     <div className={styles["wrapper-cart"]}>
+//       <button className={styles.heading} onClick={() => setIsOpen(!isOpen)}>
+//         <span className={styles.badge}>1</span>
+//         <span className={styles.text}>My Basket</span>
+//       </button>
+//       {isOpen && (
+//         <div className={styles.cart}>
+//           {cart.map((item) => (
+//             <CartItem item={item} key={item.id} />
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+// export default Cart;
+
+import { Badge, Button, Card, Drawer, Space } from "antd";
+import { FC, useState } from "react";
 import styles from "./Cart.module.scss";
 import CartItem from "./cart-item/CartItem";
 import { cart } from "@/app/data/cart.data";
 const Cart: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles["wrapper-cart"]}>
-      <button className={styles.heading}>
+      <Button
+        type="primary"
+        className={styles.heading}
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <span className={styles.badge}>1</span>
         <span className={styles.text}>My Basket</span>
-      </button>
-      <div className={styles.cart}>
-        {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
-        ))}
-      </div>
+      </Button>
+
+      <Drawer
+        title="Basic Drawer"
+        onClose={() => setIsOpen(false)}
+        open={isOpen}
+      >
+        <div className={styles.cart}>
+          {cart.map((item) => (
+            <CartItem item={item} key={item.id} />
+          ))}
+        </div>
+        <div className={styles.button_container}>
+          <Button className={styles.secondary_button}>
+            <span className={styles.text}>Checkout</span>
+          </Button>
+        </div>
+      </Drawer>
     </div>
   );
 };
